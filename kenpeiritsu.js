904bab9605c71fc6e9c5366038136265d26cfacb
YukaMuto <74124705+YukaMuto@users.noreply.github.com>
2021-02-07 11:43:01 +0900

    Add files via upload

---
+++ kenpeiritsu.js
@@ -0,0 +1,7 @@
+<script>
+alert('建ぺい率');
+var building = prompt("建物面積の値を入力してください");
+var site = prompt("敷地面積 の値を入力してください");
+document.write("入力  建物面積 = " + building + ":  敷地面積 = " + site + "<br />");
+document.write('建ぺい率は'+ building/site*100 +'％' + "<br />");
+</script>
