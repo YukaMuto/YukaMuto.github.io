000baf5e5f2b3338e9403a19b93c5159fa6c94fe
YukaMuto <74124705+YukaMuto@users.noreply.github.com>
2021-02-15 11:16:12 +0900

    Update kenpeiritsu.js

--- kenpeiritsu.js
+++ kenpeiritsu.js
@@ -1,4 +1,5 @@
 alert('建ぺい率');
 var building = prompt("建物面積の値を入力してください");
 var site = prompt("敷地面積 の値を入力してください");
-alert('建ぺい率は'+ building/site*100 +'％')
+document.write("入力  建物面積= " + building + ": 敷地面積 =" + site + "<br />");
+document.write('建ぺい率は'+ (building/site*100 +'％')
